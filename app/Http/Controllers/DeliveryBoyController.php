20c9a306b42fa68586c415eabcdd26ac77bcbdcc
Ravi <raviup198@gmail.com>
2025-03-31 13:04:02 +0530

    Delhi-Parcel

--- app/Http/Controllers/DeliveryBoyController.php
+++ app/Http/Controllers/DeliveryBoyController.php
@@ -108,7 +108,7 @@ class DeliveryBoyController extends Controller
             });
 
 
-        $toDayOrder = $ordersQuery1->where(['order_status' => 'Booked', 'assign_to' => $id])->orWhere('parcel_type', ['delivery', 'Pickup', 'Direct'])->count();
+        $toDayOrder = $ordersQuery1->where(['order_status' => 'Booked', 'assign_to' => $id])->where('parcel_type', ['delivery', 'Pickup', 'Direct'])->count();
         $toDayCompleteOrder = (clone $ordersQuery)->where('order_status', ['Delivered', 'Delivered to branch'])->count();
 
         // totalOrder details
