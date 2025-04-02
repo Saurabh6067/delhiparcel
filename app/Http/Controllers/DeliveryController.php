6631cf6ca1b47e8cdaaf6bd8dc925eaedd2ac68d
Ravi <raviup198@gmail.com>
2025-04-02 18:55:32 +0530

    Delhi-Parcel

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -699,7 +699,7 @@ class DeliveryController extends Controller
         if ($request->filter) {
             $filterType = $request->filter;
             $data = Order::where(['sender_order_status' => 'Delivered'])->whereIn('sender_order_pin', $pinCodes)->where('service_type', $filterType)->get();
-            dd($data->toArray());
+            dd($filterType->toArray());
             return response()->json([
                 'success' => true,
                 'html' => view('delivery.inc.otherTransferOrderDetails', compact('data'))->render(),
