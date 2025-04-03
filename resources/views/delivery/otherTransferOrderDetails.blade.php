a1964b42dd8c42d4d146f9c47ebbd6d114651406
Ravi <raviup198@gmail.com>
2025-04-03 17:49:49 +0530

    Delhi-Parcel

--- resources/views/delivery/otherTransferOrderDetails.blade.php
+++ resources/views/delivery/otherTransferOrderDetails.blade.php
@@ -48,8 +48,7 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        <input type="checkbox" id="select-all-assign"
-                                        onclick="toggleCheckboxes(this, 'row-checkbox1', 'btn-assign')">
+
                                         @foreach ($data as $value)
                                             @if (empty($value->assign_to))
                                                 <button class="btn btn-sm btn-light font-weight-bold btn-assign d-none"
@@ -72,7 +71,8 @@
                                             <th>#</th>
                                             <th>
                                                 <label for="select-all-assign">Assign</label>
-                                                
+                                                <input type="checkbox" id="select-all-assign"
+                                                    onclick="toggleCheckboxes(this, 'row-checkbox1', 'btn-assign')">
                                             </th>
                                             {{-- <th>
                                                 <label for="select-all-transfer">Transfer</label>
